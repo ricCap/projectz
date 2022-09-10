596b331bc367bc41ddfdc264c4af63daed561767
ricCap <capraroriccardo@gmail.com>
2022-09-10 20:23:51 +0200

    #16 admin can create project though manager contract

--- contracts/Manager.sol
+++ contracts/Manager.sol
@@ -1,24 +1,74 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity ^0.8.4;
+pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/access/AccessControl.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
 
-contract Manager is AccessControl {
-    bytes32 public constant PARTNER_ROLE = keccak256("PARTNER_ROLE");
-    bytes32 public constant UNKOWN_ROLE = keccak256("UNKOWN_ROLE");
+import "./ProjectTemplateInterface.sol";
 
-    constructor() {
-        _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
+contract Manager is AccessControl, IERC721Receiver {
+  bytes32 public constant PARTNER_ROLE = keccak256("PARTNER_ROLE");
+  bytes32 public constant UNKOWN_ROLE = keccak256("UNKOWN_ROLE");
+
+  mapping(uint256 => address) internal projectTemplateIDToAddress;
+
+  uint256 internal projectTemplatesLength = 0;
+
+  event ProjectMinted(uint256 _projectTemplate, uint256 _index);
+
+  constructor() {
+    _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
+  }
+
+  /**
+   * @dev Add a new project template
+   */
+  function addProjectTemplate(address _address)
+    public
+    onlyRole(DEFAULT_ADMIN_ROLE)
+    returns (uint256 _index)
+  {
+    // TODO check _address supports ERC721 interface
+    projectTemplateIDToAddress[projectTemplatesLength] = _address;
+    projectTemplatesLength++;
+    return projectTemplatesLength - 1;
+  }
+
+  function getRole() public view returns (bytes32) {
+    if (hasRole(DEFAULT_ADMIN_ROLE, msg.sender)) {
+      return "DEFAULT_ADMIN_ROLE";
+    } else if (hasRole(PARTNER_ROLE, msg.sender)) {
+      return "PARTNER_ROLE";
+    } else {
+      return "UNKOWN_ROLE";
     }
+  }
 
-    function getRole() public view returns (string memory) {
-        if (hasRole(DEFAULT_ADMIN_ROLE, msg.sender)) {
-            return "DEFAULT_ADMIN_ROLE";
-        } else if (hasRole(PARTNER_ROLE, msg.sender)) {
-            return "PARTNER_ROLE";
-        } else {
-            return "UNKOWN_ROLE";
-        }
-    }
+  function createProject(uint256 _templateIndex)
+    public
+    onlyRole(DEFAULT_ADMIN_ROLE)
+    returns (uint256 _index)
+  {
+    require(
+      _templateIndex < projectTemplatesLength,
+      "Invalid project template index"
+    );
+    ProjectTemplateI _template = ProjectTemplateI(
+      projectTemplateIDToAddress[_templateIndex]
+    );
+
+    _index = _template.safeMint();
+    emit ProjectMinted(_templateIndex, _index);
+  }
+
+  function onERC721Received(
+    address operator,
+    address from,
+    uint256 tokenId,
+    bytes calldata data
+  ) external override returns (bytes4) {
+    return IERC721Receiver.onERC721Received.selector;
+  }
 }
---
+++ contracts/ProjectTemplateInterface.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.9;
+
+interface ProjectTemplateI {
+  function safeMint() external returns (uint256 _tokenId);
+}
