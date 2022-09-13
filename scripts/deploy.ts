2698d770d20c529d8ae0d9ede42aa1f2d5d20068
ricCap <capraroriccardo@gmail.com>
2022-09-13 21:56:25 +0200

    resolve #20: move connect functionalities to navbar and remove marketplace

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,22 +1,19 @@
-import { ethers } from "hardhat";
+import { ethers } from 'hardhat'
 
 async function deploy(contractName: string) {
-  const factory = await ethers.getContractFactory(contractName);
-  const contract = await factory.deploy();
-  await contract.deployed();
-  console.log(`${contractName} deployed to ${contract.address}`);
+  const factory = await ethers.getContractFactory(contractName)
+  const contract = await factory.deploy()
+  await contract.deployed()
+  console.log(`${contractName} deployed to ${contract.address}`)
 }
 
 async function main() {
-  deploy("Marketplace")
-  deploy("Manager")
+  deploy('Manager')
 }
-
-
 
 // We recommend this pattern to be able to use async/await everywhere
 // and properly handle errors.
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+main().catch(error => {
+  console.error(error)
+  process.exitCode = 1
+})
